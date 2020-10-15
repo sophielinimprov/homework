ac32f1f260e13efa28922767446987578d208811
Sophie Lin <sophielin.tm@gmail.com>
2020-10-15 17:32:34 +0800

    Hahow homework3 revised 2

--- src/main/kotlin/Box.kt
+++ src/main/kotlin/Box.kt
@@ -1,6 +1,8 @@
 import java.util.*
 
 fun main(args: Array<String>) {
+    val box3 : Box = Box ("Box3",23f,14f,13)
+    val box5 : Box = Box("Box5", 39.5f,27.5f,23)
    val scanner = Scanner(System.`in`)
     print( "Please enter object's length: ")
     var length:Float=scanner.nextFloat()
@@ -11,19 +13,15 @@ fun main(args: Array<String>) {
 //    if (box3.validate(length, width, height)) println("Box3")
 //    else if (box5.validate(length, width, height)) println("Box5")
 //    else println("It's too big to contain!")
-    when(Box3().validate(length,width,height)){
-        true -> println("Box3")
-        false -> if(Box5().validate(length,width,height)) println("Box5") else println("It's too big to contain!")
+    when(box3.validate(length,width,height)){
+        true -> println("${box3.name}")
+        false -> if(box5.validate(length,width,height)) println("${box5.name}") else println("It's too big to contain!")
     }
 
 }
 
 
-class Box3 : Box(23f,14f,13)
-class Box5 : Box(39.5f,27.5f,23)
-
-
-open class Box(var length: Float, var width: Float, var height: Int){
+open class Box(val name : String,var length: Float, var width: Float, var height: Int){
     fun validate(length: Float, width:Float, height: Int) : Boolean {
         return (length<=this.length && width<=this.width && height<=this.height)
     }
